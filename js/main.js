2ae811b70fdb54b32e353b817feb5f4ef45ca4de
Lawrence Moore <lawrence2978@hotmail.com>
2014-10-23 17:00:57 -0400

    Added content page transition

--- js/main.js
+++ js/main.js
@@ -1,25 +1,56 @@
 $(function(){
     var wrapper = $('#wrapper');
     var screen = $('body, html');
+    var i = 0;
 
+    //Loads templates for homepage
     $(wrapper).append(JST.about);
     $(wrapper).append(JST.code);
     $(wrapper).append(JST.design);
     $(wrapper).append(JST.contact);
 
+    //Content Pages
+    $(wrapper).append(JST.aboutPage);
+    $(wrapper).append(JST.designPage);
+    $(wrapper).append(JST.codePage);
+    $(wrapper).append(JST.contactPage);
+
+    //Stores the content pages in a variable
+    var contentPages = $('.content-page');
+
+    console.log(contentPages);
+
+    //Expands the homepage tile when clicked after grabbing the screen dimensions
     $('.home-tile').click(function(){
         var tileExpand = $(this).children('.tile-expand');
-        var screenHeight = $(screen).height();
-        var screenWidth = $(screen).width();
 
         $(tileExpand).css({
             zIndex: 100
         });
 
         $(tileExpand).animate({
-            width: screenWidth,
-            height: screenHeight
-        })
+            width: 200 + '%',
+            height: 200 + '%'
+        }, function(){
+            var selectedDivClass = $(tileExpand).parent().attr('class');
+            var selectedClassIsolate = selectedDivClass.split(' ')[1];
+
+            //Checks the names of the content pages and compares them to the section that was click. Triggers animation after finding match
+            for(i; i < contentPages.length; ++i){
+                if(($(contentPages[i]).attr('id').split('-')[0]) == selectedClassIsolate){
+
+                    var activePage = contentPages[i];
+
+                    $(activePage).addClass('active-page');
+                    $(activePage).css({
+                        zIndex: 150
+                    });
+                    $(activePage).animate({
+                        opacity: 1
+                    }, 'fast');
+                }
+            }
+        });
     });
 });
 
--- js/templates/templates.js
+++ js/templates/templates.js
@@ -6,8 +6,20 @@ this["JST"]["about"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"ma
 
 
 
+this["JST"]["aboutPage"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+  return "<div class=\"content-page\" id=\"about-page\">\r\n  About\r\n</div>";
+  },"useData":true});
+
+
+
 this["JST"]["code"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
   return "<div class=\"home-tile code\">\r\n    <div class=\"tile-expand\">\r\n        <span class=\"icon\"></span>\r\n    </div>\r\n</div>";
+  },"useData":true});
+
+
+
+this["JST"]["codePage"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+  return "<div class=\"content-page\" id=\"code-page\">\r\n  About\r\n</div>";
   },"useData":true});
 
 
@@ -18,6 +30,18 @@ this["JST"]["contact"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"
 
 
 
+this["JST"]["contactPage"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+  return "<div class=\"content-page\" id=\"contact-page\">\r\n    Contact\r\n</div>";
+  },"useData":true});
+
+
+
 this["JST"]["design"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
   return "<div class=\"home-tile design\">\r\n    <div class=\"tile-expand\">\r\n        <span class=\"icon\"></span>\r\n    </div>\r\n</div>";
+  },"useData":true});
+
+
+
+this["JST"]["designPage"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+  return "<div class=\"content-page\" id=\"design-page\">\r\n  About\r\n</div>";
   },"useData":true});
