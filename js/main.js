248de57c04eb36f9359a0c2849c592e89b514399
Lawrence Moore <lawrence2978@hotmail.com>
2014-10-23 15:19:07 -0400

    Added homepage expand functionality

--- js/main.js
+++ js/main.js
@@ -1,9 +1,25 @@
 $(function(){
     var wrapper = $('#wrapper');
+    var screen = $('body, html');
 
     $(wrapper).append(JST.about);
     $(wrapper).append(JST.code);
     $(wrapper).append(JST.design);
     $(wrapper).append(JST.contact);
+
+    $('.home-tile').click(function(){
+        var tileExpand = $(this).children('.tile-expand');
+        var screenHeight = $(screen).height();
+        var screenWidth = $(screen).width();
+
+        $(tileExpand).css({
+            zIndex: 100
+        });
+
+        $(tileExpand).animate({
+            width: screenWidth,
+            height: screenHeight
+        })
+    });
 });
 
--- js/templates/templates.js
+++ js/templates/templates.js
@@ -1,23 +1,23 @@
 this["JST"] = this["JST"] || {};
 
 this["JST"]["about"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-  return "<div class=\"home-tile about\">\r\n    <span class=\"icon\"></span>\r\n</div>";
+  return "<div class=\"home-tile about\">\r\n    <div class=\"tile-expand\">\r\n        <span class=\"icon\"></span>\r\n    </div>\r\n</div>";
   },"useData":true});
 
 
 
 this["JST"]["code"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-  return "<div class=\"home-tile code\">\r\n    <span class=\"icon\"></span>\r\n</div>";
+  return "<div class=\"home-tile code\">\r\n    <div class=\"tile-expand\">\r\n        <span class=\"icon\"></span>\r\n    </div>\r\n</div>";
   },"useData":true});
 
 
 
 this["JST"]["contact"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-  return "<div class=\"home-tile contact\">\r\n    <span class=\"icon\"></span>\r\n</div>";
+  return "<div class=\"home-tile contact\">\r\n    <div class=\"tile-expand\">\r\n        <span class=\"icon\"></span>\r\n    </div>\r\n</div>";
   },"useData":true});
 
 
 
 this["JST"]["design"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-  return "<div class=\"home-tile design\">\r\n    <span class=\"icon\"></span>\r\n</div>";
+  return "<div class=\"home-tile design\">\r\n    <div class=\"tile-expand\">\r\n        <span class=\"icon\"></span>\r\n    </div>\r\n</div>";
   },"useData":true});
