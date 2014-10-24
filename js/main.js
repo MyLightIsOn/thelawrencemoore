785976e9e45ecb1ec08007f8bb83d29997a2af7a
Lawrence Moore <lawrence2978@hotmail.com>
2014-10-24 12:31:07 -0400

    Added page content transitions and images

--- js/main.js
+++ js/main.js
@@ -10,8 +10,8 @@ $(function(){
 
     //Content Pages
     $(wrapper).append(JST.aboutPage);
-    $(wrapper).append(JST.designPage);
     $(wrapper).append(JST.codePage);
+    $(wrapper).append(JST.designPage);
     $(wrapper).append(JST.contactPage);
 
     //Stores the content pages in a variable
@@ -40,42 +40,95 @@ $(function(){
                 if(($(contentPages[i]).attr('id').split('-')[0]) == selectedClassIsolate){
 
                     var activePage = contentPages[i];
-                    console.log(contentPages[i])
+                    var pageContent = $(activePage).find('.page-content');
+                    var headerText =  $(activePage).find('.page-header h1');
+                    var headerBottom = $(activePage).find('.page-header h5');
+                    var bottom = $(headerBottom).position().top+$(headerBottom).outerHeight(true);
+
                     $(activePage).addClass('active-page');
                     $(activePage).css({
                         zIndex: 150
                     });
                     $(activePage).animate({
                         opacity: 1
-                    }, 'fast');
+                    }, 300);
+                    $(activePage).css({
+                        overflow: 'visible'
+                    });
+
+
+                    $(window).one('scroll', function(){
+                        $(activePage).find('.page-wrapper').animate({
+                            opacity: 1,
+                            top: 0
+                        });
+
+                        $(pageContent).css({
+                            top: bottom + 130
+                        });
+                        $(pageContent).animate({
+                            opacity: 1,
+                            top: bottom + 60
+                        });
+
+                        $(headerText).css({
+                            zIndex: 100
+                        });
+                        $(headerBottom).css({
+                            zIndex: 100
+                        });
+                    })
                 }
             }
         });
     });
 
+
+    //Close Home Page tiles
     $('.close-page').on('click', function(){
         var expandedTile = $("#wrapper").find('.expanded');
         var activeContentPage = $("#wrapper").find('.active-page');
+        var pageWrapper = $(activeContentPage).find('.page-wrapper');
+        var headerText =  $(activeContentPage).find('.page-header h1');
+        var headerBottom = $(activeContentPage).find('.page-header h5');
 
-        $(activeContentPage).animate({
-            opacity: 0
-        }, function(){
-            $(this).css({
-                zIndex: -100
-            });
+        $(screen).animate(
+            { scrollTop: 0 }, 'fast', returnState()
+        );
 
-            $(expandedTile).animate({
-                width: 100 + '%',
-                height: 100 + '%'
+        function returnState(){
+            $(activeContentPage).animate({
+                opacity: 0
             }, function(){
-                $(expandedTile).css({
+                $(this).css({
+                    zIndex: -100
+                });
+
+                $(expandedTile).animate({
+                    width: 100 + '%',
+                    height: 100 + '%'
+                }, function(){
+                    $(expandedTile).css({
+                        zIndex: 0
+                    });
+                });
+
+                $(activeContentPage).css({
+                    overflow: 'hidden'
+                });
+
+                $(pageWrapper).css({
+                    opacity: 0
+                });
+
+                $(headerText, headerBottom).css({
                     zIndex: 0
                 });
+                $(activeContentPage).removeClass('active-page');
+                $(expandedTile).removeClass('expanded');
             });
+        }
 
-            $(activeContentPage).removeClass('active-page');
-            $(expandedTile).removeClass('expanded');
-        });
     })
 });
 
